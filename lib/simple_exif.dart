6099de856eb702796ec0e1580b46977b016bffb6
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2024-12-15 18:21:13 +0900

    Under development.

--- lib/simple_exif.dart
+++ lib/simple_exif.dart
@@ -1,2 +1,3 @@
+export 'src/enum_default_exif_tags.dart';
 export 'src/exif_reader.dart';
 export 'src/exif_writer.dart';
--- lib/src/enum_default_exif_tags.dart
+++ lib/src/enum_default_exif_tags.dart
@@ -1,6 +1,6 @@
 /// (en) An Enum that defines default Exif tags.
 ///
 /// (ja) Exifのデフォルトタグを定義したEnum。
-enum EnumDefaultTags{
+enum EnumDefaultExifTags{
 
 }
